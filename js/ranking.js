639627adedb876a6c3028bc357559c725fce455c
Felipe Ortega da Silva <felipeortegadasilvaoficial@gmail.com>
2025-10-09 12:59:29 -0300

    Correção de pequenas falhas

--- js/ranking.js
+++ js/ranking.js
@@ -1,9 +1,9 @@
 let jogadores = JSON.parse(localStorage.getItem("usuarios")).sort(
-  (ranking_maior, ranking_menor) => ranking_maior.pontos - ranking_menor.pontos,
+  (ranking_maior, ranking_menor) => ranking_menor.pontos - ranking_maior.pontos,
 );
 for (let index = 0; index < jogadores.length; index++) {
   let div = document.createElement("div");
-  div.innerHTML = `<p>${jogadores[index].nome}</p>  <p> ${jogadores[index].pontos}</p>`;
+  div.innerHTML = `<p>${jogadores[index].nome}</p>  <p>${jogadores[index].pontos}</p>;`;
   div.classList.add("celula");
   document.getElementById("tabela").appendChild(div);
 }
